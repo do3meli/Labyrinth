2e3ccd8d3e00f2e2a4692e092e6d8079611a6511
bueti <bueti@offroadfreunde.ch>
2013-03-03 11:19:59 +0100

    labels, buttons, panels...

--- src/main/java/ch/zhaw/labyrinth/gui/InitGui.java
+++ src/main/java/ch/zhaw/labyrinth/gui/InitGui.java
@@ -18,24 +18,38 @@ public class InitGui {
 
     public void initGui() {
         // Variables
-        JLabel createLabel;
-        JLabel solveLabel;
-        JLabel createLabyrinth;
+        JLabel createLabel = new JLabel("Create Algorithm");
+        JLabel solveLabel = new JLabel("Solve Algorithm");
+        JLabel createLabyrinth = new JLabel("Create Labyrinth");
 
-        JButton startButton;
-        JButton pauseButton;
-        JButton resetButton;
+        JButton startButton = new JButton("Start");
+        JButton pauseButton = new JButton("Pause");
+        JButton resetButton = new JButton("Reset");
 
-        JPanel labyrinthPanel;
-        JPanel configPanel;
+        JPanel labyrinthPanel = new JPanel();
+        JPanel configPanel = new JPanel();
         JPanel buttonPanel;
+
+        String[] createAlgorithms = { "Depth-First", "Prim", "Kruskal"};
+        JComboBox createList = new JComboBox(createAlgorithms);
+        String[] solveAlgorithms = { "Wall-Follower", "Right-Hand", "Trémaux", "Backtrack", "Shortest Path"};
+        JComboBox solveList = new JComboBox(solveAlgorithms);
 
         frame = new JFrame("LabyrinthSolver");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Container contentPane = frame.getContentPane();
-        contentPane.setLayout(new BorderLayout());
 
+        Container contentPane = frame.getContentPane();
+        contentPane.setLayout(new GridLayout(2,2));
+
+        labyrinthPanel.setLayout(new BorderLayout());
+        configPanel.setLayout(new GridLayout(2,4));
+        configPanel.add(createLabel,0,0);
+        configPanel.add(createList,0,1);
+        configPanel.add(solveLabel);
+        configPanel.add(solveList);
+        contentPane.add(configPanel);
+        contentPane.add(labyrinthPanel);
 
         frame.setSize(800, 500);
         frame.setVisible(true);
