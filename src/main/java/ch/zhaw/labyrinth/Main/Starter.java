293b13427b30c9d19ad0560b1f54284f81b62a1d
bueti <bueti@offroadfreunde.ch>
2013-03-03 10:58:46 +0100

    starter

---
+++ src/main/java/ch/zhaw/labyrinth/Main/Starter.java
@@ -0,0 +1,17 @@
+package ch.zhaw.labyrinth.Main;
+
+import ch.zhaw.labyrinth.gui.InitGui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bbu
+ * Date: 03.03.13
+ * Time: 10:56
+ */
+public class Starter {
+    public static void main(String[] args) {
+        // Initialize GUI
+        InitGui gui = new InitGui();
+
+    }
+}
