b9c22467d1c2a1e18220d706fe91a00ceaed4a4e
bueti <bueti@offroadfreunde.ch>
2013-03-05 09:38:21 +0100

    Point to square, first ry

--- src/main/java/ch/zhaw/labyrinth/gui/Point.java
+++ src/main/java/ch/zhaw/labyrinth/gui/Point.java
@@ -4,18 +4,15 @@ import java.awt.*;
 
 public class Point {
     public final int x1;
-    public final int x2;
     public final int y1;
-    public final int y2;
 
     public Point(int x1, int y1) {
         this.x1 = x1;
-        this.x2 = x1+100;
         this.y1 = y1;
-        this.y2 = y1;
     }
 
     public void paint(Graphics g) {
-        g.drawLine(this.x1, this.y1, this.x2, this.y2);
+        g.drawLine(this.x1, this.y1, this.x1+100, this.y1);
+        g.drawLine(this.x1, this.y1-100, this.x1+100, this.y1-100);
     }
 }
