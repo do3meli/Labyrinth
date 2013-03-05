0ca5917a756c37b56a7d5dd745beab97360860f0
Dom <info@dominicschlegel.ch>
2013-03-05 09:16:36 +0100

    formatting adjusted

--- src/main/java/ch/zhaw/labyrinth/Main/Starter.java
+++ src/main/java/ch/zhaw/labyrinth/Main/Starter.java
@@ -15,7 +15,7 @@ public class Starter {
     public static void main(String[] args) {
 
         LabyrinthBuilder lab = new DeepFirstSearch(20);
-
+ 
         int[][] a= lab.getMaze();
         for(int i=0; i<20; i++) {
             for (int j=0; j<20; j++) {
