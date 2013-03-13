8546503e05fd7cfbaab9beadfc0fe4f8b8dc57d0
Dom <info@dominicschlegel.ch>
2013-03-13 21:08:30 +0100

    empty Kruskal.java

---
+++ src/main/java/ch/zhaw/labyrinth/builder/Kruskal.java
@@ -0,0 +1,13 @@
+package ch.zhaw.labyrinth.builder;
+
+public class Kruskal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
