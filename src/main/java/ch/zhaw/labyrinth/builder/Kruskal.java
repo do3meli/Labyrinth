2c6a36bd7f63d59b83616a6fa68828f3d796f096
Dom <info@dominicschlegel.ch>
2013-03-19 20:03:07 +0100

    reflected removal of Tree.java

--- src/main/java/ch/zhaw/labyrinth/builder/Kruskal.java
+++ src/main/java/ch/zhaw/labyrinth/builder/Kruskal.java
@@ -1,12 +1,18 @@
 package ch.zhaw.labyrinth.builder;
 
+
+/**
+ * 
+ * @author Dominic Schlegel
+ *
+ */
 public class Kruskal extends LabyrinthBuilder{
 
-	public Kruskal(int dim) {
-		super(dim);
-		// TODO Auto-generated constructor stub
-	}
-
 	
-
+	public Kruskal(int dim) {
+		// call super constructor
+		super(dim);
+		
+		
+	}
 }
