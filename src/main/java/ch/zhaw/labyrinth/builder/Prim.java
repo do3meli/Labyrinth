b63e8c33a2bfe59c82a11adb68ce3567c5cb2486
Dom <info@dominicschlegel.ch>
2013-03-19 20:26:08 +0100

    started prim implementation with http://www.cs.washington.edu/education/courses/cse326/07su/prj2/kruskal.html

--- src/main/java/ch/zhaw/labyrinth/builder/Prim.java
+++ src/main/java/ch/zhaw/labyrinth/builder/Prim.java
@@ -1,10 +1,60 @@
 package ch.zhaw.labyrinth.builder;
 
+import java.util.Arrays;
+
 public class Prim extends LabyrinthBuilder {
 
+	// vars
+	private int[][] mazevisits;
+	
+	// constans
+	private static final int VISITED = 1;
+	
 	public Prim(int dim) {
+		// call super constructor
 		super(dim);
-		// TODO Auto-generated constructor stub
+		// create int array for track of visited cells
+		this.mazevisits = new int[getDimension()][getDimension()];
+		// fill mazevisited array
+		fillArray();
+		// now create the maze
+		createMaze();
+		
+	}
+	
+	
+	
+	private void createMaze() {
+		// choose random cell
+		int x = getRandomIntOdd(getDimension());
+		int y = getRandomIntOdd(getDimension());
+		
+		// mark this cell now as visited
+		this.mazevisits[x][y] = VISITED;
+		
+		while(!allVisited()) {
+			
+		}
+		
+	}
+	
+	private boolean allVisited(){
+		for(int x = 0 ; x < 3 ; x++){
+			for(int y = 0 ; y < 3 ; y++){
+			      if(this.mazevisits[x][y] == 0){
+			    	  return false;
+			      }
+			}
+		}
+		return true;
 	}
 
+
+	public void fillArray(){
+		for(int x=0; x < this.mazevisits.length; x++){
+		    Arrays.fill( this.mazevisits[x], 0 );
+		}
+	}
+	
+	
 }
