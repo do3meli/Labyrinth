6a70d5e1efe0dd0e4937c36700e19ff897542c0f
Dom <info@dominicschlegel.ch>
2013-04-27 23:07:06 +0200

    Kruskal now extends new Labyrinth structure

--- src/main/java/ch/zhaw/labyrinth/builder/Kruskal.java
+++ src/main/java/ch/zhaw/labyrinth/builder/Kruskal.java
@@ -1,12 +1,14 @@
 package ch.zhaw.labyrinth.builder;
 
+import ch.zhaw.labyrinth.utils.Labyrinth;
+
 
 /**
  * 
  * @author Dominic Schlegel
  *
  */
-public class Kruskal extends LabyrinthBuilder{
+public class Kruskal extends Labyrinth{
 
 	
 	public Kruskal(int dim) {
