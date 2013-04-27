b973013661dd5b804c6b62a0dd3d878b6dca2a3b
Dom <info@dominicschlegel.ch>
2013-04-27 23:24:40 +0200

    added Cell unit test

---
+++ src/test/java/ch/zhaw/labyrinth/utils/TestCell.java
@@ -0,0 +1,44 @@
+package ch.zhaw.labyrinth.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestCell {
+	
+	
+	// instance variables
+	private static Cell c;
+	
+	
+	// NEW methods with the annotation BeforeClass are executed
+    // before executing all tests
+    // Note: this method must be static
+    @BeforeClass
+    public static void beforeEverything() {
+    	c = new Cell();
+    }
+	
+	
+	@Test
+	public void testSetPath() {
+		
+		c.setPath(true);
+		assertEquals(true,c.isPath());
+		c.setPath(false);
+		assertEquals(false,c.isPath());
+		
+	}
+	
+	@Test
+	public void testSetVisited() {
+		
+		c.setVisited(true);
+		assertEquals(true,c.isVisited());
+		c.setVisited(false);
+		assertEquals(false,c.isVisited());
+		
+	}
+
+}
