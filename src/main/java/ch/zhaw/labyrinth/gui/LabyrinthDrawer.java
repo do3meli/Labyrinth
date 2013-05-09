0526f3dc3fdba8149e9ffd806c58bf7e4b4709c7
bueti <bueti@offroadfreunde.ch>
2013-05-09 10:06:14 +0200

    fixed drawing of x- and y-axes

--- src/main/java/ch/zhaw/labyrinth/gui/LabyrinthDrawer.java
+++ src/main/java/ch/zhaw/labyrinth/gui/LabyrinthDrawer.java
@@ -58,7 +58,7 @@ public class LabyrinthDrawer extends JPanel implements Runnable {
     	
         if (!curCell.isPath()){
             g.setColor(Color.black);
-        	g.fillRect(curCoordinate.getY()*zoom,curCoordinate.getX()*zoom,1*zoom,1*zoom );
+        	g.fillRect(curCoordinate.getX()*zoom,curCoordinate.getY()*zoom,1*zoom,1*zoom );
         	
         }
       
