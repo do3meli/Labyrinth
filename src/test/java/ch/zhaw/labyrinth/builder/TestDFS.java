851465f3e93120a03966251e14be8d408635587f
Dom <info@dominicschlegel.ch>
2013-05-10 16:19:54 +0200

    DFS test now creates a DFS object

--- src/test/java/ch/zhaw/labyrinth/builder/TestDFS.java
+++ src/test/java/ch/zhaw/labyrinth/builder/TestDFS.java
@@ -14,7 +14,7 @@ public class TestDFS {
     // Note: this method must be static
     @BeforeClass
     public static void beforeEverything() {
-    	dfs = new Labyrinth(23);
+    	dfs = new DepthFirstSearch(23);
     }
     
     @Test
