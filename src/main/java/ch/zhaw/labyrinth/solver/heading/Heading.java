4a87fd1f1aac9ece5177f58283af0860333f4101
bueti <bueti@offroadfreunde.ch>
2013-05-11 22:38:31 +0200

    JavaDoc

--- src/main/java/ch/zhaw/labyrinth/solver/heading/Heading.java
+++ src/main/java/ch/zhaw/labyrinth/solver/heading/Heading.java
@@ -5,28 +5,78 @@ import ch.zhaw.labyrinth.utils.Labyrinth;
 
 /**
  * This interface defines how to move with a specific heading.
+ *
+ * @author b.buetikofer
  */
 public interface Heading {
 
-    // Check methods if is a wall
+    /**
+     * This method checks if the cell right of itself is a path
+     * @return true if path
+     */
     boolean isRight();
+
+    /**
+     * This method checks if the cell left of itself is a path
+     * @return true if path
+     */
     boolean isLeft();
+
+    /**
+     * This method checks if the cell straight ahead of itself is a path
+     * @return true if path
+     */
     boolean isStraight();
+
+    /**
+     * This method checks if the cell behind of itself is a path
+     * @return true if path (should always be true!)
+     */
     boolean isBack();
 
-    // Check if the cell was already visited
-    int getVisits(int x, int y);
+    /**
+     * Marks a cell as visited
+     *
+     * @param x x-coordinate
+     * @param y y-coordinate
+     * @return true if visited
+     */
+    boolean isVisited(int x, int y);
 
-    // Methods to move
+    /**
+     * Turns right and goes on step ahead
+     * @return the new heading
+     */
     Heading goRight();
+
+    /**
+     * Turns left and goes on step ahead
+     * @return the new heading
+     */
     Heading goLeft();
+
+    /**
+     * Goes one step ahead
+     * @return the new heading (should always be the same as before)
+     */
     Heading goStraight();
+
+    /**
+     * Turns around and goes on step ahead
+     * @return the new heading
+     */
     Heading goBack();
 
-    // Get Coordinate
+    /**
+     *
+     * @return the new coordinate
+     */
     Coordinate getCoordinate();
 
-    // Get solved maze
-    Labyrinth getSolvedLabyrinth();
+    /**
+     *
+     * @return the solvedMaze Map
+     */
+    Labyrinth getSolvedMaze();
 
 }
