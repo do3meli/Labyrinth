76a07f3d242be8ddc8450ed228a0a650b40c5bb2
bueti <bueti@offroadfreunde.ch>
2013-05-17 20:09:49 +0200

    bugfixing

--- src/main/java/ch/zhaw/labyrinth/solver/AStar.java
+++ src/main/java/ch/zhaw/labyrinth/solver/AStar.java
@@ -38,19 +38,27 @@ public class AStar extends Observable implements Solver {
         Coordinate exit = maze.getExit();
 
 
-        Coordinate currentCoordinate = entry;
-        int x = currentCoordinate.getX();
-        int y = currentCoordinate.getY();
+        Coordinate startCoordinate = entry;
+        int x = startCoordinate.getX();
+        int y = startCoordinate.getY();
 
         /**
          * Add start point to the openSet, set predecessor to self
          */
-        openSet.addPath(currentCoordinate, currentCoordinate);
+        openSet.addPath(startCoordinate, startCoordinate);
 
         /**
          * Dive into the recursion!! YAY
          *
          */
+        Coordinate currentCoordinate = openSet.getLowestF();
+
+        /**
+         * add current cell to the closed set and remove it from the maze
+         */
+        closedSet.addPath(currentCoordinate);
+        openSet.removeCell(currentCoordinate);
+
         while ( !currentCoordinate.equals(exit) ) {
             /**
              * Add all reachable neighbors to the openSet and add calculate f-cost
@@ -89,11 +97,6 @@ public class AStar extends Observable implements Solver {
                 checkValue(x - 1, y + 1, currentCoordinate, G_DIAGONAL);
             }
 
-            /**
-             * add current cell to the closed set and remove it from the maze
-             */
-            closedSet.addPath(currentCoordinate);
-            openSet.removeCell(currentCoordinate);
 
             /**
              * Get cell with the lowest F value
@@ -101,6 +104,13 @@ public class AStar extends Observable implements Solver {
             currentCoordinate = openSet.getLowestF();
             x=currentCoordinate.getX();
             y=currentCoordinate.getY();
+
+            /**
+             * add current cell to the closed set and remove it from the maze
+             */
+            closedSet.addPath(currentCoordinate);
+            openSet.removeCell(currentCoordinate);
+
 
         }
 
@@ -194,44 +204,3 @@ public class AStar extends Observable implements Solver {
         currentCell.setF(currentCell.getG() + currentCell.getH());
     }
 }
-
-
-/**
- function A*(start,goal)
- closedset := the empty set    // The set of nodes already evaluated.
- openset := {start}    // The set of tentative nodes to be evaluated, initially containing the start node
- came_from := the empty map    // The map of navigated nodes.
-
- g_score[start] := 0    // Cost from start along best known path.
- // Estimated total cost from start to goal through y.
- f_score[start] := g_score[start] + heuristic_cost_estimate(start, goal)
-
- while openset is not empty
- current := the node in openset having the lowest f_score[] value
- if current = goal
- return reconstruct_path(came_from, goal)
-
- remove current from openset
- add current to closedset
- for each neighbor in neighbor_nodes(current)
- tentative_g_score := g_score[current] + dist_between(current,neighbor)
- if neighbor in closedset
- if tentative_g_score >= g_score[neighbor]
- continue
-
- if neighbor not in openset or tentative_g_score < g_score[neighbor]
- came_from[neighbor] := current
- g_score[neighbor] := tentative_g_score
- f_score[neighbor] := g_score[neighbor] + heuristic_cost_estimate(neighbor, goal)
- if neighbor not in openset
- add neighbor to openset
-
- return failure
-
- function reconstruct_path(came_from, current_node)
- if current_node in came_from
- p := reconstruct_path(came_from, came_from[current_node])
- return (p + current_node)
- else
- return current_node
- */
