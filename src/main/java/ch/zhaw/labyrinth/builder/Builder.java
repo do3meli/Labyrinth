4eb2feeb5424dc0e6c058b48d8368fe48c7e48c4
Dom <info@dominicschlegel.ch>
2013-05-19 18:47:27 +0200

    javadoc Builder

--- src/main/java/ch/zhaw/labyrinth/builder/Builder.java
+++ src/main/java/ch/zhaw/labyrinth/builder/Builder.java
@@ -2,6 +2,11 @@ package ch.zhaw.labyrinth.builder;
 
 import ch.zhaw.labyrinth.utils.Labyrinth;
 
+/**
+ * Interface that makes sure build() method is getting implemented
+ * in all builder algorithms
+ * @author d.schlegel
+ */
 public interface Builder {
 	 /**
      * This method implements the builder algorithm
